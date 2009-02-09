14c7d4dae01732743bbb74dc671d2086b441e3d3
George Ornbo <george@shapeshed.com>
2009-02-09 22:31:17 +0000

    adding delete

--- rsyncbackup.sh
+++ rsyncbackup.sh
@@ -3,10 +3,10 @@
 # A simple local backup using rsync
 # Syncronises with a server on the local network over SSH
  
-rsync -azc -e ssh /Users/george/documents/accounts george@bomberg.local:/mnt/dionysus/ 
-rsync -azc -e ssh /Users/george/documents/clients george@bomberg.local:/mnt/dionysus/ 
-rsync -azc -e ssh /Users/george/documents/commercial george@bomberg.local:/mnt/dionysus/ 
-rsync -azc -e ssh /Users/george/documents/icons george@bomberg.local:/mnt/dionysus/ 
-rsync -azc -e ssh /Users/george/documents/knowledge george@bomberg.local:/mnt/dionysus/ 
-rsync -azc -e ssh /Users/george/documents/potential_clients george@bomberg.local:/mnt/dionysus/ 
+rsync -azc -e ssh --delete /Users/george/documents/accounts george@bomberg.local:/mnt/dionysus
+rsync -azc -e ssh --delete /Users/george/documents/clients george@bomberg.local:/mnt/dionysus
+rsync -azc -e ssh --delete /Users/george/documents/commercial george@bomberg.local:/mnt/dionysus
+rsync -azc -e ssh --delete /Users/george/documents/icons george@bomberg.local:/mnt/dionysus
+rsync -azc -e ssh --delete /Users/george/documents/knowledge george@bomberg.local:/mnt/dionysus
+rsync -azc -e ssh --delete /Users/george/documents/potential_clients george@bomberg.local:/mnt/dionysus
 echo 'All Done. Time for Bed.'
