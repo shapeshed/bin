7cc33c36f64680880d36e1efdde78de1c4d20583
George Ornbo <george@shapeshed.com>
2009-02-10 09:26:15 +0000

    extracting dirs to array to make it more extensible

--- rsyncbackup.sh
+++ rsyncbackup.sh
@@ -2,11 +2,29 @@
 
 # A simple local backup using rsync
 # Syncronises with a server on the local network over SSH
- 
-rsync -azc -e ssh --delete /Users/george/documents/accounts george@bomberg.local:/mnt/dionysus
-rsync -azc -e ssh --delete /Users/george/documents/clients george@bomberg.local:/mnt/dionysus
-rsync -azc -e ssh --delete /Users/george/documents/commercial george@bomberg.local:/mnt/dionysus
-rsync -azc -e ssh --delete /Users/george/documents/icons george@bomberg.local:/mnt/dionysus
-rsync -azc -e ssh --delete /Users/george/documents/knowledge george@bomberg.local:/mnt/dionysus
-rsync -azc -e ssh --delete /Users/george/documents/potential_clients george@bomberg.local:/mnt/dionysus
-echo 'All Done. Time for Bed.'
+
+# Array of directories to be backed up
+BACKUPDIRS=("/Users/george/documents/accounts"
+"/Users/george/documents/clients"
+"/Users/george/documents/commercial"
+"/Users/george/documents/icons"
+"/Users/george/documents/knowledge"
+"/Users/george/documents/potential_clients")
+
+# Destination server for backups
+DESTINATION="george@bomberg.local:/mnt/dionysus"
+
+# Logging function
+function log() {
+   echo "[`date +'%a %b %d %T %Y'`] [$$] $1" >> ~/bin/log/rsyncbackup.log
+}
+
+# Loop through directories and do the backup
+for DIR in ${BACKUPDIRS[*]}
+do
+	rsync -azc -e ssh --delete $DIR $DESTINATION
+	log "$DIR OK"
+done
+
+
+
